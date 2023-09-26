c78337a347e5e3d84d2be72915c11b075a50f79f
vkopallx <Venkata.Kopalle@solidigm.com>
2023-09-26 16:52:55 +0530

    modified security for vpn

--- vpc-peering/security-group.tf
+++ vpc-peering/security-group.tf
@@ -32,25 +32,28 @@ resource "aws_security_group" "vpc2_sg" {
   description = "allowing all traffic"
   vpc_id      = aws_vpc.vpc2.id
 
-  ingress {
-    description = "Allowing all traffic"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    description = "Allowing all traffic"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
   tags = merge(
     var.common_tags,
     {
       Name = "VPC-2|Open"
     }
   )
+}
+
+resource "aws_security_group_rule" "vpc2_sg_ssh" {
+  type                     = "ingress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.vpc1_sg.id
+  security_group_id        = aws_security_group.vpc2_sg.id
+}
+
+resource "aws_security_group_rule" "vpc2_sg_egress" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.vpc2_sg.id
 }
