18d2df22347d7cb662a17baa422b8cc03b812b08
vkopallx <Venkata.Kopalle@solidigm.com>
2023-10-10 18:07:13 +0530

    modified robo-infra and created vpc module

---
+++ robo-infra/VPC/variable.tf
@@ -0,0 +1,28 @@
+variable "vpc_cidrs" {
+  default = { "vpc1" = "10.0.0.0/16",
+  "vpc2" = "172.0.0.0/16" }
+}
+
+variable "azs" {
+  default = ["us-east-1a", "us-east-1b"]
+}
+
+variable "vpc1_public_subnets" {
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "vpc1_private_subnets" {
+  default = {}
+}
+
+variable "vpc2_public_subnets" {
+  default = ["172.0.1.0/24", "172.0.2.0/24"]
+}
+
+variable "vpc2_private_subnets" {
+  default = ["172.0.3.0/24", "172.0.4.0/24"]
+}
+
+variable "vpc2_database_subnets" {
+  default = ["172.0.5.0/24", "172.0.6.0/24"]
+}
