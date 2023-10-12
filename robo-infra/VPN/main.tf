7ded79970e5e54c8034ba8157188f68ba8d7dc30
vkopallx <Venkata.Kopalle@solidigm.com>
2023-10-12 21:00:33 +0530

    VPC

--- robo-infra/VPN/main.tf
+++ robo-infra/VPN/main.tf
@@ -1,4 +1,15 @@
 module "vpc_infra" {
-  source = "../VPC"
-
+  source                = "../VPC"
+  vpc_cidrs             = var.vpc_cidrs
+  vpc1_public_subnets   = var.vpc1_public_subnets
+  vpc1_private_subnets  = var.vpc1_private_subnets
+  vpc1_database_subnets = var.vpc1_database_subnets
+  vpc2_public_subnets   = var.vpc2_public_subnets
+  vpc2_private_subnets  = var.vpc2_private_subnets
+  vpc2_database_subnets = var.vpc2_database_subnets
+  common_tags           = var.common_tags
 }
+
+# resource "aws_vpc_peering_connection" "vpc1_vpc2" {
+#   peer_vpc_id = 
+# }
